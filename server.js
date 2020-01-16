8f5ec8ce0884d95844c800df43a6bff6e45ca8b4
Widi Sumardy <ws.rainstar@gmail.com>
2020-01-17 01:26:33 +0700

    heroku configuration

--- server.js
+++ server.js
@@ -6,4 +6,4 @@ const path = require('path')
 app.use('/static', express.static(path.join(__dirname, 'resource')))
 app.get('/', (req, res) => res.sendFile(path.join(__dirname, '/index.html')))
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.listen(port, () => console.log(`App is listening on port ${port}!`))
