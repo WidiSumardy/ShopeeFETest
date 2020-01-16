4cb9d38c61caca13ced01eebd31f75bed57f8a23
Widi Sumardy <ws.rainstar@gmail.com>
2020-01-17 01:38:02 +0700

    Update server.js

--- server.js
+++ server.js
@@ -1,9 +1,8 @@
 const express = require('express')
 const app = express()
-const port = 3000
 const path = require('path')
 
 app.use('/static', express.static(path.join(__dirname, 'resource')))
 app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'index.html')))
 
-app.listen(port, () => console.log(`App is listening on port ${port}!`))
+app.listen(process.env.PORT || 3000, () => console.log(`App is listening!`))
